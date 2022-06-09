34b61ffd860640ea4abe74235241a4ebd744612d
JasmiRani <106468684+JasmiRani@users.noreply.github.com>
2022-06-09 10:39:19 +0530

    Add files via upload

---
+++ hello.py
@@ -0,0 +1,14 @@
+
+#Check the flow of the program and give output….
+if (True):
+    print("hello")
+if (True):
+    print("hii")
+elif (True):
+      print("Byee")
+if (True):
+    print("bye")
+else:
+    print("oho")
+if (True):
+    print("i am again")
